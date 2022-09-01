51f62ff98aa45474976aac8e16c369ffec386382
perfection1st <elijah@0v3rKill-PC.localdomain>
2022-08-31 19:52:04 -0500

    Added feature 2

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-// Create alternating black/red tiles - Feature 1
+// Feature 2 created random color on every other square
+let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+
 for(let i = 0; i < 81; i++) {
   var tile = document.createElement('div');
   tile.classList.add('tile');
@@ -11,7 +13,7 @@ for(let i = 0; i < 81; i++) {
 
   } else {
     tile.classList.add('odd');
-    tile.style.backgroundColor = "red";
+    tile.style.backgroundColor = randomColor;
   }
   document.body.appendChild(tile);
 
