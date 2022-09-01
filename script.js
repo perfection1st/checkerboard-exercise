d0e2868968ab0e9e5f4a32a7040cb39c0ce75154
perfection1st <elijah@0v3rKill-PC.localdomain>
2022-08-31 22:20:00 -0500

    Add Feature 4: Every tile gets new color every 2 seconds

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 // Feature 2 created random color on every other square
-function generateRandomColor() {
-  return "#" + Math.floor(Math.random()*16777215).toString(16);
-}
+let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
 
 
 for(let i = 0; i < 81; i++) {
-  var tile = document.createElement('div');
-  tile.classList.add('tile');
+  var createTile = document.createElement('div');
+  createTile.classList.add('tile');
+  document.body.appendChild(createTile);
+}
+
+for(i = 0; i < 81; i++) {
+  let tile = document.getElementsByClassName('tile')[i];
   tile.style.cssFloat = "left";
   tile.style.minWidth = "11.1%";
   tile.style.paddingBottom = "11.1%";
-  if(i % 2 === 0) {
-    tile.classList.add('even');
-    tile.style.backgroundColor = generateRandomColor();
-
-  } else {
-    tile.classList.add('odd');
-    tile.style.backgroundColor = generateRandomColor();
-  }
-  document.body.appendChild(tile);
-
+  tile.style.backgroundColor = "#" + (Math.floor(Math.random()*16777215).toString(16));
+  setInterval(() => {
+    tile.style.backgroundColor = "#" + (Math.floor(Math.random()*16777215).toString(16)); 
+  }, 2000);
 }
